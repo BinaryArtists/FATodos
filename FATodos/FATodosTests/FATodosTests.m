35ddc6b6b3f7a934cdd39cf99b2af499c5e0fdcc
fallen.ink <fengzilijie@gmail.com>
2015-10-09 15:11:25 +0800

    集成测试框架 kiwi

--- FATodos/FATodosTests/FATodosTests.m
+++ FATodos/FATodosTests/FATodosTests.m
@@ -9,32 +9,46 @@
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
-@interface FATodosTests : XCTestCase
+#import "kiwi.h"
 
-@end
+//@interface FATodosTests : XCTestCase
+//
+//@end
+//
+//@implementation FATodosTests
+//
+//- (void)setUp {
+//    [super setUp];
+//    // Put setup code here. This method is called before the invocation of each test method in the class.
+//}
+//
+//- (void)tearDown {
+//    // Put teardown code here. This method is called after the invocation of each test method in the class.
+//    [super tearDown];
+//}
+//
+//- (void)testExample {
+//    // This is an example of a functional test case.
+//    XCTAssert(YES, @"Pass");
+//}
+//
+//- (void)testPerformanceExample {
+//    // This is an example of a performance test case.
+//    [self measureBlock:^{
+//        // Put the code you want to measure the time of here.
+//    }];
+//}
+//
+//@end
 
-@implementation FATodosTests
+SPEC_BEGIN(MathSpec)
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
+describe(@"Math", ^{
+    it(@"is pretty cool", ^{
+        NSUInteger a = 16;
+        NSUInteger b = 26;
+        [[theValue(a + b) should] equal:theValue(42)];
+    });
+});
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
-
-@end
+SPEC_END
---
+++ FATodos/Pods/Target Support Files/Pods-FATodosTests/Pods-FATodosTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FATodosTests : NSObject
+@end
+@implementation PodsDummy_Pods_FATodosTests
+@end
